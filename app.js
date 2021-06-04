c321a7c46b93c1ec958167531ae71223c68abe4e
pmita <panos.mita2@outlook.com>
2021-06-04 14:23:38 +0100

    toggle navigation added

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log('it works');
+const burgerBtn = document.querySelector('.burger-btn');
+const navLinks = document.querySelector('.nav-links');
+
+
+
+//Event Listeners
+burgerBtn.addEventListener( 'click', function(){
+    navLinks.classList.toggle('nav-active');
+})
